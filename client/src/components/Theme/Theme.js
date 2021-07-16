1fdbf028480028e2a5cff62f78b02920c579b5ae
Kody Samaroo <github email address>
2021-07-16 13:44:34 -0400

    adding themes

--- client/src/components/Theme/Theme.js
+++ client/src/components/Theme/Theme.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Select from 'react-select';
 import './Theme.css';
 
-const Theme = () => (
+const Theme = () => {
+    const [theme, setTheme] = useState("")
+
+    const options = [
+        { value: 'red', label: "Apple" },
+        { value: 'orange', label: "Orange" },
+        { value: 'yellow', label: "Pineapple" },
+        { value: 'green', label: "Lime" },
+        { value: 'blue', label: "Blueberry" },
+        { value: 'white', label: "Neutral" },
+        { value: 'black', label: "Noir" },
+        { value: 'dark', label: "Dark Mode" }
+    ]
+
+    return (
     <div className="theme">
-        Hello
+        <Select
+            className="theme-select"
+            placeholder="Select a theme"
+            value={theme}
+            onChange={(event) => setTheme(event.value)}
+            options={options}
+        />
     </div>
-)
+    )
+}
 
 export default Theme;
